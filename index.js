6abc405a9ef61850953253f63c70405e15aef50e
Bradley Bossard <bradleybossard@gmail.com>
2016-11-02 16:34:40 +0000

    Adding function for getting revisionId

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 const fetch = require('fetch');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 const filename = 'index.html';
+
+function getRevision(callback) {
+  const revisionCmd = "cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'";
+  const child = exec(revisionCmd,  function (err, stdout, stderr) {
+    if (err !== null) {
+      console.log('exec error: ' + err);
+    }
+    const revision = stdout.trim();
+    callback(revision);
+    //return revision;
+  });
+}
+
+getRevision(function(revision) {
+  console.log(revision);
+});
+
+function readFile() {
+  // Read the file and print its contents.
+  fs.readFile(filename, 'utf8', function(err, body) {
+    if (err) throw err;
+
+    let $ = cheerio.load(body);
+    //console.log($('table:nth-of-type(2)').html());
+    const $table = $('table:nth-of-type(2)');
+    $table.find('tr').each(function(i, el) {
+      const $td = $(el).find('td');
+      const revision = $($td.get(0)).text().trim();
+      const releaseDate = $($td.get(1)).text().trim();
+      const model = $($td.get(2)).text().trim();
+      const pcbRevision = $($td.get(3)).text().trim();
+      const memory = $($td.get(4)).text().trim();
+      const notes = $($td.get(5)).text().trim();
+      //console.log(revision, releaseDate, model, pcbRevision, memory, notes);
+    });
+
+  });
+}
+
+//readFile();
+
 /*
 const url = 'http://elinux.org/RPi_HardwareHistory#Board_Revision_History';
 fetch.fetchUrl(url, function(err, meta, body) {
@@ -16,23 +58,3 @@ fetch.fetchUrl(url, function(err, meta, body) {
 });
 */
 
-// Read the file and print its contents.
-fs.readFile(filename, 'utf8', function(err, body) {
-  if (err) throw err;
-
-	let $ = cheerio.load(body);
-	//console.log($('table:nth-of-type(2)').html());
-	const $table = $('table:nth-of-type(2)');
-  $table.find('tr').each(function(i, el) {
-    const $td = $(el).find('td');
-    //console.log($($td.get(0)).text());
-    const revision = $($td.get(0)).text().trim();
-    const releaseDate = $($td.get(1)).text().trim();
-    const model = $($td.get(2)).text().trim();
-    const pcbRevision = $($td.get(3)).text().trim();
-    const memory = $($td.get(4)).text().trim();
-    const notes = $($td.get(5)).text().trim();
-    console.log(revision, releaseDate, model, pcbRevision, memory, notes);
-	});
-
-});
